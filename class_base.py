90c1406f3dd83a02d9ae434b83881870dffa6817
young <godsky1990@gmail.com>
2020-04-27 23:25:06 +0900

    20.04.27 study

--- class_base.py
+++ class_base.py
@@ -1,13 +1,56 @@
 
-
-
-
-class businessCard():
+class Account:
+        num_accounts = 0
         
-    def inits(self,name,email,addr):
-        self.name = name
-        self.dmail = email
-        self.addr = addr
+        def __init__(self,name):
+            self.name = name
+            Account.num_accounts += 1
+
+        def __del__(self):
+            Account.num_accounts -= 1           
+            
+class Stock:
+        market = "kospi"
+        
+class txt_number:
+        def open_num(self):
+                f = open("number_list.txt","wt")
+                list_num = []
+                for x in range(0,10,1):
+                        list_num.append(x)
+                        
+
+                       
+                f.write(str(list_num))
+
+                f.close
+
+                d = open("number_list.txt","rt")
+                lines = d.readlines()
+                print(lines)
+                
+class Point:
+
+        def __init__(self,x,y):
+                self.x = x
+                self.y = y
+                
+        def setx(self, x):
+                self.x = x     
+
+        def sety(self, y):
+                self.y = y
+
+        def get(self):
+                return(self.x, self.y)
+        
+        def move(self,dx,dy):
+                self.x += dx
+                self.y += dy
+
+                return(self.x,self.y)
+                
+class businessCard():
 
 
     def print_info(self,name,email,addr):
--- study_young.py
+++ study_young.py
@@ -2,14 +2,46 @@
 import os
 import class_base
 
+
 #현재경로 가져오기
 os.getcwd()
 
 
 #함수의 인자로 리스트를받은 후 리스트 내에 있는 모든 정숫값에 대한 최댓값과 최솟값을 반환하는 함수를 작성하세요.
 
-fh = class_base.businessCard()
-fh.get_max_min()
+#fh = class_base.businessCard()
+#Ac = class_base()
 
-cb = class_base.businessCard()
-cb.print_info('young','young90','korea_city')
+#fh.get_max_min()
+
+#cb = class_base.businessCard()
+#cb.print_info('young','young90','korea_city')
+
+
+#ac1 = Ac.Account("young1990")
+#print(ac1.name)
+
+cp = class_base.Point(12,56)
+
+
+print(cp.get())
+print(cp.move(12,43))
+
+a = class_base.Stock()
+a.market
+
+f = open('buy_list.txt','rt')
+lines = f.readlines()
+f.close
+for line in lines:
+    print(line, end="")
+
+d = open('sel_list.txt', 'wt')
+d.write('삼성전자\n')
+d.write('현대차\n')
+d.close()
+
+print("-------------------------------")
+
+ct = class_base.txt_number()
+ct.open_num()
